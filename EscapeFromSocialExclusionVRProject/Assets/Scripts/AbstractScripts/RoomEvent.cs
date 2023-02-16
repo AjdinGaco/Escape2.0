b70e94d9810ea5b6c30ab89dc35e341b3318e0b1
Ajdin Gaco <ajdin.gaco@student.ap.be>
2023-02-16 16:30:10 +0100

    Move VRproject To Unique Foldeer

---
+++ EscapeFromSocialExclusionVRProject/Assets/Scripts/AbstractScripts/RoomEvent.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class RoomEvent : MonoBehaviour
+{
+    public List<GameObject> gameObjectsToEnableOnStart;
+    public List<GameObject> gameObjectsToDisableOnEnd;
+    public bool condition = false;
+
+    public RoomDirector roomDirector;
+    public void EventUpdate()
+    {
+        if (condition == true)
+        {
+            this.EndEvent();
+        }
+        else
+        {
+            this.OnUpdate();
+        }
+    }
+    /// <summary>
+    /// This update will only run if the current event is the one as in RoomDirector 
+    /// </summary>
+    public virtual void OnUpdate()
+    {
+
+    }
+
+
+    public void StartEvent()
+    {
+        foreach (GameObject go in gameObjectsToEnableOnStart)
+        {
+            go.gameObject.active = true;
+        }
+        OnStart();
+    }
+    /// <summary>
+    /// This code will run once during the start of the event. 
+    /// </summary>
+    public virtual void OnStart()
+    {
+        //Custom Code
+    }
+    public void EndEvent()
+    {
+        foreach (GameObject go in gameObjectsToDisableOnEnd)
+        {
+            go.gameObject.active = false;
+        }
+        OnEnd();
+        roomDirector.CurrentEventDone();
+    }
+    /// <summary>
+    /// This code will run once during the End of the event. 
+    /// </summary>
+    public virtual void OnEnd()
+    {
+        //Custom Code
+    }
+
+
+}
---
+++ EscapeFromSocialExclusionVRProject/Assets/Scripts/MainScripts/RoomDirector.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomDirector : MonoBehaviour
+{
+    
+    public List<RoomEvent> events = new List<RoomEvent>();
+    private RoomEvent roomEvent;
+    public int currentEvent = -1;
+
+    void Start()
+    {
+        NextEvent();
+    }
+    private void NextEvent()
+    {
+
+
+        currentEvent += 1;
+        if (events[currentEvent])
+        {
+            roomEvent = events[currentEvent];
+            roomEvent.roomDirector = this;
+            roomEvent.StartEvent();
+        }
+    }
+    public void CurrentEventDone()
+    {
+        NextEvent();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (roomEvent)
+            roomEvent.EventUpdate();
+    }
+}
