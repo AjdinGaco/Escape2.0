3e5d148e740915217188e56552ed6c3f573f5036
Ajdin Gaco <ajdin.gaco@student.ap.be>
2023-03-29 01:28:16 +0200

    Popup Issue Fixes

--- EscapeFromSocialExclusionVRProject/Assets/Scripts/ImagePuzzle.cs
+++ EscapeFromSocialExclusionVRProject/Assets/Scripts/ImagePuzzle.cs
@@ -28,6 +28,7 @@ public class ImagePuzzle : RoomPuzzle
         }
     }
     public int completedPieces;
+    public float turnDuration = 0.5f; // Set the duration of the turning animation in seconds
     public float turnCooldown = 0.5f; // Set the cooldown period in seconds
 
     private bool canTurn = true; // Flag to indicate whether a puzzle piece can be turned
@@ -37,23 +38,36 @@ public class ImagePuzzle : RoomPuzzle
         if (canTurn)
         {
             // Turn the puzzle piece by 90 degrees around the z-axis
-            piece.Rotate(Vector3.forward, 90f);
+            Quaternion targetRotation = piece.rotation * Quaternion.Euler(0f, 0f, 90f);
+            StartCoroutine(TurnAnimation(piece, targetRotation));
 
             // Set the flag to prevent turning for the cooldown period
             canTurn = false;
 
             // Start the cooldown coroutine
             StartCoroutine(TurnCooldown());
+        }
+    }
 
-            // Check if all puzzle pieces have been correctly oriented
-            completedPieces = 0;
-            foreach (GameObject gameObject in puzzlepieces)
-            {
-                if (gameObject.transform.localRotation.eulerAngles.z < 0.001f)
-                {
-                    completedPieces++;
-                }
-            }
+    private IEnumerator TurnAnimation(Transform piece, Quaternion targetRotation)
+    {
+        float elapsed = 0f;
+        Quaternion initialRotation = piece.rotation;
+
+        while (elapsed < turnDuration)
+        {
+            elapsed += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsed / turnDuration);
+            piece.rotation = Quaternion.Slerp(initialRotation, targetRotation, t);
+            yield return null;
+        }
+
+        piece.rotation = targetRotation;
+
+        // Check if the puzzle piece is correctly oriented
+        if (Mathf.Abs(piece.localRotation.eulerAngles.z) < 0.001f)
+        {
+            completedPieces++;
             if (completedPieces >= puzzlepieces.Count)
             {
                 completion = true;
@@ -63,6 +77,9 @@ public class ImagePuzzle : RoomPuzzle
                 }
             }
         }
+
+        // Debug the rotation after turning
+        Debug.Log("Rotation after turning: " + piece.localRotation.eulerAngles.z);
     }
 
     private IEnumerator TurnCooldown()
--- EscapeFromSocialExclusionVRProject/Assets/Scripts/PopupMaster.cs
+++ EscapeFromSocialExclusionVRProject/Assets/Scripts/PopupMaster.cs
@@ -8,11 +8,12 @@ public class PopupMaster : MonoBehaviour
     public Vector3 offset; // The offset from the camera
     public float rotationSpeed = 2.0f; // The speed of rotation
 
-    private Camera mainCamera;
+    public GameObject mainCamera;
 
     private void Start()
     {
-        mainCamera = Camera.main;
+        if (mainCamera == null)
+            mainCamera = Camera.main.gameObject;
     }
 
     public void PopUpImage(Sprite sprite)
@@ -25,6 +26,7 @@ public class PopupMaster : MonoBehaviour
             popupObj = Instantiate(popupPrefab, this.transform);
             popupObj.GetComponent<PopUpController>().Sprite = sprite;
             popupObj.transform.position = mainCamera.transform.position + mainCamera.transform.forward * offset.z;
+            popupObj.transform.LookAt(mainCamera.transform);
         }
     }
 
@@ -32,7 +34,7 @@ public class PopupMaster : MonoBehaviour
     {
         if (popupObj != null)
         {
-            popupObj.transform.LookAt(mainCamera.transform);
+            
 
             // Calculate the direction from the camera to the popup object
             Vector3 toPopup = popupObj.transform.position - mainCamera.transform.position;
@@ -40,10 +42,11 @@ public class PopupMaster : MonoBehaviour
             // Calculate the angle between the camera forward direction and the direction to the popup object
             float angle = Vector3.Angle(toPopup, mainCamera.transform.forward);
 
-            if (angle > 45.0f)
+            if (angle > 90.0f)
             {
                 // If the popup object is behind the camera, move it in front
                 popupObj.transform.position = mainCamera.transform.position + mainCamera.transform.forward * offset.z;
+                popupObj.transform.LookAt(mainCamera.transform);
             }
             else
             {
