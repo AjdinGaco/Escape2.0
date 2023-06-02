69e8586f260ef4d5100140519c5f4e01a8fb4b65
Ajdin Gaco <ajdin.gaco@student.ap.be>
2023-06-02 15:06:38 +0200

    Add 4 types of vision impairments.

---
+++ EscapeFromSocialExclusionVRProject/Assets/BlindnessManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlindnessManager : MonoBehaviour
+{
+    public Image EffectsCanvas;
+    public VisionDisabilities currentActive;
+    public Sprite CataractsSprite, DiabeticRetinopathySprite, GlaucomaSprite, MacularDegenerationSprite;
+
+    public void ChangeVisionDisabilities(VisionDisabilities visionDisability)
+    {
+        currentActive = visionDisability;
+        switch (visionDisability)
+        {
+            case VisionDisabilities.None:
+                EffectsCanvas.sprite = null;
+                break;
+            case VisionDisabilities.Cataracts:
+                EffectsCanvas.sprite = CataractsSprite;
+                break;
+            case VisionDisabilities.DiabeticRetinopathy:
+                EffectsCanvas.sprite = DiabeticRetinopathySprite;
+                break;
+            case VisionDisabilities.Glaucoma:
+                EffectsCanvas.sprite = GlaucomaSprite;
+                break;
+            case VisionDisabilities.MacularDegeneration:
+                EffectsCanvas.sprite = MacularDegenerationSprite;
+                break;
+        }
+    }
+}
+
+public enum VisionDisabilities
+{
+    None,
+    Cataracts,
+    DiabeticRetinopathy,
+    Glaucoma,
+    MacularDegeneration
+}
